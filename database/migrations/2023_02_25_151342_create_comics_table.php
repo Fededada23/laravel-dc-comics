5b038cae86f35e701e8be7f62066c2f2a2ac081b
Fededada23 <dadamofederico@gmail.com>
2023-02-25 16:36:15 +0100

    created migrate e seeder

---
+++ database/migrations/2023_02_25_151342_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 15);
+            $table->string('series', 60);
+            $table->date('sale_date', 20)->nullable();
+            $table->string('type', 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
